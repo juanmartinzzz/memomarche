5ae3468a8c534049c0559c00e307b8d996bcc6cc
Juan Martínez <juan.martinzzz@gmail.com>
2024-05-30 21:23:28 -0400

    First commit - perform voice-to-text, send message to Telegram as either shopping list item, or general reminder.

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import mic_stream
+import speech_to_text_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MicStreamPlugin.register(with: registry.registrar(forPlugin: "MicStreamPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+}
